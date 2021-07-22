8962ba4031e46a5a2d1b8a1c8bbf5f74e296b66f
Cecilia <ysycecilia@gmail.com>
2021-07-22 21:44:13 +0000

    add a sold out badge if quantity is zero

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
 
   def show
+    # find only works with ID, and returns a single entity
+    # .where(color: 'black'): gives all
+    # .find_by(color: 'black'): gives first result
     @category = Category.find(params[:id])
     # raise @category.inspect  // will tell what category is used for dubugging
     @products = @category.products.order(created_at: :desc)
