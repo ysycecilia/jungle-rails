2eb39e877ced52fcb3615447c3be3967803b85d6
Cecilia <ysycecilia@gmail.com>
2021-07-23 02:15:59 +0000

    create admin auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_AUTH_USERNAME"], password: ENV["ADMIN_AUTH_PASSWORD"]
+  # if: -> { request.subdomain == "admin" }
 
   def index
     @products = Product.order(id: :desc).all
